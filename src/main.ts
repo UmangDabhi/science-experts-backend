9ccd4998b3318e3076f189a93c80469c284df37f
Umang Dabhi <ud30104@gmail.com>
2025-04-10 01:05:39 +0530

    port env added

--- src/main.ts
+++ src/main.ts
@@ -3,16 +3,22 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { AllExceptionsFilter } from './Helper/all-exceptions.filter';
 import { ResponseInterceptor } from './interceptors/response.intercepter';
-import { join } from 'path';
+import { config } from 'dotenv'; // Import dotenv
+
+config(); // Load environment variables
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     cors: true,
   });
+
   app.setGlobalPrefix('api');
   app.useGlobalPipes(new ValidationPipe());
   app.useGlobalInterceptors(new ResponseInterceptor());
   app.useGlobalFilters(new AllExceptionsFilter());
-  await app.listen(5000);
+
+  const port = process.env.BACKEND_PORT || 3000; // Read from env, fallback to 3000
+  await app.listen(port);
+  console.log(`🚀 Server running on http://localhost:${port}`);
 }
 bootstrap();
