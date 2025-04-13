765daeb6dedcfd6982cebb8e80c5de408ebd0fbc
Umang Dabhi <ud30104@gmail.com>
2025-04-13 14:06:16 +0530

    new build url change

---
+++ build/assets/pageNotFound-DTAF1SMj.js
@@ -0,0 +1,1 @@
+import{j as o}from"./index-CRih-8pH.js";const e=()=>o.jsx("div",{children:"PageNotFound"});export{e as default};
--- build/assets/pageNotFound-DgY9DQo4.js
+++
@@ -1,1 +0,0 @@
-import{j as o}from"./index-pR6HjcrU.js";const e=()=>o.jsx("div",{children:"PageNotFound"});export{e as default};
