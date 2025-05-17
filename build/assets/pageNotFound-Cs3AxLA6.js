35eb8ea0dfc28d3f4f570916ded288f107f5fc1c
Umang Dabhi <ud30104@gmail.com>
2025-05-17 16:17:28 +0530

    new build added

--- build/assets/pageNotFound-Cs3AxLA6.js
+++
@@ -1,1 +0,0 @@
-import{j as o}from"./index-COGDzE6e.js";const e=()=>o.jsx("div",{children:"PageNotFound"});export{e as default};
--- build/assets/uploadFile-aNihOkik.js
+++
@@ -1,1 +0,0 @@
-import{am as o,a_ as l,ao as s,ap as e}from"./index-COGDzE6e.js";const r=o("upload/File",e,a=>l(s.UPLOAD_FILE,a),!0);export{r as u};
