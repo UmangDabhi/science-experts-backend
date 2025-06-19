04e66ee3014a55a8fe7f5d08e559fc7a3063b391
Umang Dabhi <ud30104@gmail.com>
2025-06-19 23:27:33 +0530

    new changes adsense and new build

---
+++ build/assets/ActionButtons-DpDOhPG0.js
@@ -0,0 +1,5 @@
+import{r as l,I as d,_ as h,j as t}from"./index-DDTZZVyy.js";import{T as v}from"./index-pJqgb8zV.js";var x={icon:{tag:"svg",attrs:{viewBox:"64 64 896 896",focusable:"false"},children:[{tag:"path",attrs:{d:"M360 184h-8c4.4 0 8-3.6 8-8v8h304v-8c0 4.4 3.6 8 8 8h-8v72h72v-80c0-35.3-28.7-64-64-64H352c-35.3 0-64 28.7-64 64v80h72v-72zm504 72H160c-17.7 0-32 14.3-32 32v32c0 4.4 3.6 8 8 8h60.4l24.7 523c1.6 34.1 29.8 61 63.9 61h454c34.2 0 62.3-26.8 63.9-61l24.7-523H888c4.4 0 8-3.6 8-8v-32c0-17.7-14.3-32-32-32zM731.3 840H292.7l-24.2-512h487l-24.2 512z"}}]},name:"delete",theme:"outlined"},m=function(a,e){return l.createElement(d,h({},a,{ref:e,icon:x}))},f=l.forwardRef(m);const g=({buttons:s=[],className:a=""})=>t.jsx("div",{className:`flex flex-wrap gap-2 ${a}`,children:s.map(({text:e,onClick:c,icon:n,color:r={},tooltip:i},o)=>t.jsx(v,{title:i||e,children:t.jsxs("button",{onClick:c,className:`flex items-center gap-1 px-3 py-1 text-sm 
+            ${r.text||"text-black"} 
+            ${r.bg||"bg-gray-100"} 
+            ${r.hover||"hover:bg-gray-200"} 
+            rounded-full transition duration-300`,children:[n&&t.jsx(n,{}),e]})},o))});export{g as A,f as R};
